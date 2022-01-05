acf0d290bf4b61f79f87fd8274e5427617df247f
Willian Marciano <willian_marciano@hotmail.com>
2022-01-05 16:58:30 -0300

    Modelando a controller de Fornecedores

--- src/MarcianoIO.Api/Configuration/DependencyInjectionConfig.cs
+++ src/MarcianoIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,7 @@
-﻿using MarcianoIO.Business.Interface;
+﻿using MarcianoIO.Business.Intefaces;
+using MarcianoIO.Business.Interface;
+using MarcianoIO.Business.Notificacoes;
+using MarcianoIO.Business.Services;
 using MarcianoIO.Data.Context;
 using MarcianoIO.Data.Repository;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +14,13 @@ namespace MarcianoIO.Api.Configuration
         {
             services.AddScoped<BancoContexto>();
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+
             return services;
         }
     }
