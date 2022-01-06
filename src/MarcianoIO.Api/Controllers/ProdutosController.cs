4b033ee29942e082407f29db40ed60137c236b76
Willian Marciano <willian_marciano@hotmail.com>
2022-01-06 14:54:32 -0300

    Incluido ProdutoController

---
+++ src/MarcianoIO.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,106 @@
+﻿using AutoMapper;
+using MarcianoIO.Api.ViewModels;
+using MarcianoIO.Business.Intefaces;
+using MarcianoIO.Business.Interface;
+using MarcianoIO.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MarcianoIO.Api.Controllers
+{
+    [Route("api/produtos")]
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepsitory;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(IProdutoRepository produtoRepsitory,
+                                  IProdutoService produtoService,
+                                  IMapper mapper,
+                                  INotificador notificador) : base(notificador)
+        {
+            _produtoRepsitory = produtoRepsitory;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+            => _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepsitory.ObterProdutosFornecedores());
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> ObterPorId(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+            if (produtoViewModel == null) return NotFound();
+            return produtoViewModel;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> Adicionar(ProdutoViewModel produtoViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var imagemNome = Guid.NewGuid() + "_" + produtoViewModel.Imagem;
+
+            if(!UploadArquivo(produtoViewModel.ImagemUpload, imagemNome))            
+                return CustomResponse(produtoViewModel);
+            
+            produtoViewModel.Imagem = imagemNome;
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            return CustomResponse(produtoViewModel);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Adicionar(Guid id, ProdutoViewModel produtoViewModel)
+        {
+            if (id != produtoViewModel.Id)
+            {
+                NotificarErro("O id informado não é o mesmo que foi passado na query");
+                return CustomResponse(produtoViewModel);
+            }
+
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+            await _produtoService.Atualizar(_mapper.Map<Produto>(produtoViewModel));
+            return CustomResponse(produtoViewModel);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Excluir(Guid id)
+        {
+            var produto = await ObterProduto(id);
+            if (produto == null) return NotFound();
+            await _produtoService.Remover(id);
+            return CustomResponse(produto);
+        }
+
+        private bool UploadArquivo(string arquivo, string imgNome)
+        {
+            var imageDataByteArray = Convert.FromBase64String(arquivo);
+            if(string.IsNullOrEmpty(arquivo))
+            {
+                //ModelState.AddModelError(string.Empty, "Forneça uma imagem para este produto!");
+                NotificarErro("Forneça uma imagem para este produto!");
+                return false;
+            }
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgNome);
+            if(System.IO.File.Exists(filePath))
+            {
+                NotificarErro("Já existe um arquivo com este nome!");
+                return false;
+            }
+
+            System.IO.File.WriteAllBytes(filePath, imageDataByteArray);
+            return true;
+        }
+
+        private async Task<ProdutoViewModel> ObterProduto(Guid id) 
+            => _mapper.Map<ProdutoViewModel>(await _produtoRepsitory.ObterProdutoFornecedor(id));
+    }
+}
